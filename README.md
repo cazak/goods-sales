3df81c317b5abc76af61a0d82d42123d208534a1
cazak <goripov.zayan@gmail.com>
2024-08-29 12:57:02 +0700

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+## Запуск
+
+```shell
+git clone git@github.com:cazak/projects.git your-folder-name
+
+cd ./your-folder-name/
+
+make init
+```
+
+Предварительно настроить `./.env.local`, `./docker/.env`, `./docker/mysql/.env`
---
+++ config/bundles.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+
+return [
+    FrameworkBundle::class => ['all' => true],
+    DoctrineBundle::class => ['all' => true],
+    DoctrineMigrationsBundle::class => ['all' => true],
+];
